4cac1217cc679aee89132470f4b153c134649265
NaelAtouani <nael.atouani2020@gmail.com>
2023-10-23 18:46:26 +0100

    uplading video player

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import DashBoardPage from "./Pages/DashboardPage/DashBoardPage.jsx";
+import 'react-range-slider-input/dist/style.css';
+
 function App() {
 
   return (
--- src/Components/BottomBar/BottomBar.jsx
+++ src/Components/BottomBar/BottomBar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import {Box} from "@mui/material"
 import AddIcon from '@mui/icons-material/Add';
+import "./BottomBar.css"
+import RangeSlider from 'react-range-slider-input';
+
 const BottomBar = () => {
     return (
     <Box sx={{
@@ -17,7 +20,7 @@ const BottomBar = () => {
         pl: 4,pr :4
     }}>
             <AddIcon sx={{ fontSize: 20 ,backgroundColor :"#2A2E31",p:1,borderRadius:"5px",cursor : "pointer","&:hover": { backgroundColor: "#6200FC" } }}/>
-
+                <RangeSlider  id="range-slider"/>
             <AddIcon sx={{ fontSize: 20,backgroundColor :"#2A2E31",p:1,borderRadius:"5px",cursor : "pointer","&:hover": { backgroundColor: "#6200FC" }  }}/>
     </Box>
     )
